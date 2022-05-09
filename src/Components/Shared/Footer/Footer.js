e89fc37d21f9cd56825f57f2546950fd76f551ef
Siydul Islam <siydulislam333@gmail.com>
2022-05-09 14:19:53 +0600

    footer updated

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BsGoogle, BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
     return (
-        <div className="text-center mt-5">
-            <p>Copyright © 2022 by Siydul Islam | All Rights Reserved</p>
+        <div className="bg-light mt-5" style={{ height: '300px' }}>
+            <Container>
+                <div className="container row">
+                    <div className="text-dark text-center mt-5 col-md-6">
+                        <h2>ProTutor</h2>
+                        <p>Dhaka, Bangladesh</p>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center mt-5 col-md-6 text-dark">
+                        <h1><BsGoogle></BsGoogle></h1>
+                        <h1 className='mx-5'><BsFacebook></BsFacebook></h1>
+                        <h1><BsGithub></BsGithub></h1>
+                        <h1 className='mx-5'><BsLinkedin></BsLinkedin></h1>
+                    </div>
+                </div>
+                <p className="text-dark text-center mt-5">copyright @2022 Siydul Islam Sojib</p>
+            </Container>
         </div>
     );
 };
